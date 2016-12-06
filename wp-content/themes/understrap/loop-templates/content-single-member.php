1344424ec1702fa15c02783747cf02a0939d3052
CaitlinJD <caitlin_dyk@hotmail.com>
2016-12-06 15:48:24 -0500

    #011.02 Adding meta fields

--- wp-content/themes/understrap/loop-templates/content-single-member.php
+++ wp-content/themes/understrap/loop-templates/content-single-member.php
@@ -8,25 +8,37 @@
 ?>
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
     <div class="img-bkg" style="background: url('<?php echo get_the_post_thumbnail( 'rectangle-thumb' ) ?>') center left/cover no-repeat"></div>
+    <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
     <header class="entry-header">
         <p>BOARD MEMBER</p>
         <p><?php echo get_post_meta( get_the_ID(), 'bio_position', true ); ?> </p>
 
         <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 
+        <p><?php echo get_post_meta( get_the_ID(), 'employment_position', true ); ?> </p>
+        <p><?php echo get_post_meta( get_the_ID(), 'employment', true ); ?></p>
+
         <div class="entry-meta">
 
-            <?php understrap_posted_on(); ?>
+            <?php $key_1_value = get_post_meta( get_the_ID(), 'website_url', true );
+            // check if the custom field has a value
+            if ( ! empty( $key_1_value ) ) : ?>
+            <img src="<?php echo get_blog_info('template_url')?>/">
 
         </div><!-- .entry-meta -->
 
     </header><!-- .entry-header -->
 
-    <?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
     <div class="entry-content">
 
-        <?php the_content(); ?>
+        <p><?php echo get_post_meta( get_the_ID(), 'fave_store', true ); ?> </p>
+        <p><?php echo get_post_meta( get_the_ID(), 'fave_clothing', true ); ?> </p>
+        <p><?php echo get_post_meta( get_the_ID(), 'style_icon', true ); ?> </p>
+        <p><?php echo get_post_meta( get_the_ID(), 'fave_website', true ); ?> </p>
+        <p><?php echo get_post_meta( get_the_ID(), 'fave_designer', true ); ?> </p>
+
+        <h2>" "</h2>
 
         <?php
         wp_link_pages( array(
