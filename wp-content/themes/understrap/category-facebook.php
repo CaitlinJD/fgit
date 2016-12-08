b79b9772d0182c8a1e2b929f6268dd5a63dd0b12
Charlie Mayer <cmayerproductions@gmail.com>
2016-12-08 16:14:22 -0500

    updates.

--- wp-content/themes/understrap/category-facebook.php
+++ wp-content/themes/understrap/category-facebook.php
@@ -7,7 +7,55 @@
 
             <?php while ( have_posts() ) : the_post(); ?>
 
-                <?php get_template_part( 'loop-templates/content', 'archive' ); ?>
+
+
+
+                <div class="container">
+
+                  <div class="row" >
+                    <div class="col-md-4 card"><img  src="http://media.vogue.com/r/pass/2016/02/19/15-street-style-phil-oh-day-8-fw16.jpg">
+                    <p class="card-text">New York Fashion Week is over, but the street style lives on. Here are some of our favorite moments from outside the shows.</p>
+                    <a href="https://www.facebook.com/fgi.toronto/?fref=ts class="card-link">Read More +</a>
+
+                    </div>
+                    <div class="col-md-4 card">
+                    <img src="https://media.timeout.com/images/102862539/image.jpg">
+                    <p class="card-text">Check back daily for the latest looks on the street during New York Fashion Week.</p>
+                  <a href="https://www.facebook.com/fgi.toronto/?fref=ts" class="card-link">Read More +</a>
+                 </div>
+
+                      <div class="col-md-4 card">
+                      <img src="http://blog.trucco.es/wp-content/uploads/2011/09/IMG_2618.jpg">
+                      <p class="card-text">Note to bloggers who change head-to-toe, paid-to-wear outfits every hour: Please stop. Find another business. You are heralding the death of style.”</p>
+                    <a href="https://www.facebook.com/fgi.toronto/?fref=ts" class="card-link">Read More +</a>
+                   </div>
+                    </div>
+              </div>
+                <div class="row">
+                   <div class="col-md-4 card">
+                   <img src="http://blog.trucco.es/wp-content/uploads/2011/09/IMG_2747.jpg">
+                    <p class="card-text">As Milan Fashion Week drew to a close, Vogue‘s editorial team penned a roundtable discussion looking back on the week’s events. </p>
+                  <a href="https://www.facebook.com/fgi.toronto/?fref=ts" class="card-link">Read More +</a></div>
+
+                <div class="col-md-4 card">
+                <img src="http://images1.miaminewtimes.com/imager/the-cast-of-miami-social-sorah-d/u/original/6527876/miamisociallouis.jpg">
+                 <p class="card-text">The cast of Miami Social (Sorah Daiha, Ariel Stein, Katrina Campins, George French, Maria Lankina, Michael Cohen, and Hardy Hill) celebrated at the series-premiere party at Louis Bar-Lounge at the Gansevoort South this past July 9.</p>
+                 <a href="https://www.facebook.com/fgi.toronto/?fref=ts" class="card-link">Read More +</a>
+                </div>
+             <div class="col-md-4 card">
+             <img src="http://chicstylista.com/wp-content/uploads/2013/11/MG_6696-1024x785.jpg">
+              <p class="card-text">Miami Fashion Network Launch Event Fashion Blogger </p>
+              <a href="https://www.facebook.com/fgi.toronto/?fref=ts" class="card-link">Read More +</a>
+          </div>
+      </div>
+
+  </div>
+
+
+
+
+
+
 
                 <?php //the_post_navigation(); ?>
 
--- wp-content/themes/understrap/js/facebook.js
+++ wp-content/themes/understrap/js/facebook.js
@@ -1,13 +1,19 @@
 jQuery(document).ready(function($){
-  alert("sdfgasdf");
    // Ajax call
-   var url = "https://graph.facebook.com/v2.8/44589077752?fields=id%2Cname%2Cposts%7Bpicture%2Cstory%2Ccreated_time%2Cid%7D%2Cphotos&access_token=EAACEdEose0cBAPEnCWyZCI3p2lGMuG6MlCQTZC7WKsXyEPsZCCx31z63bdeZCDpZAQ0vLcxc9w5vOpWf45zCJVbcR5n0WLdSFZB2ePUmmOAoBKeTD5DrEIC8UDjlQAqM3o6doWyiinoGwNAksGt8iiokKRD3XnouM74Cpt5x7o3AZDZD";
+   var url = "https://graph.facebook.com/v2.8/44589077752?fields=id%2Cname%2Cposts%7Bpicture%2Cstory%2Ccreated_time%2Cid%7D%2Cphotos&access_token=EAAZAtGjzyjcABAIcHoR2QPLkLGHY8rJtGdrSN6412dJb3iNOTxZAV9ksIzTJyD1Dbi4XW78MKcKpCzNboAq4LEFxJ5XEkNqGhf5QcdrmORviQoBETERhO0JhZCqHvSoEKCFerpmUO6MMIro2OGZCDH8GCzZBbvuuuq56ZChP0V5QZDZD";
    $.ajax({
        url: url,
        method: 'GET',
    }).done(function(result) {
        $(".loader").show();
-       console.log("result");
+       for ( var counter=0;counter<result.posts.data.length -1; counter++){
+          console.log (result.posts.data[counter]);
+          var currentObject= result.posts.data[counter];
+          console.log (currentObject.picture) ;
+          console.log (currentObject.story) ;
+
+       }
+
 
    }).fail(function(err) {
        throw err;
