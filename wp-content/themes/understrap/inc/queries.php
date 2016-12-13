92b55586dba5220f268ba0e67ec052d6b338e7d5
CaitlinJD <caitlin_dyk@hotmail.com>
2016-12-13 15:20:41 -0500

    Changed query so the menus show up

--- wp-content/themes/understrap/inc/queries.php
+++ wp-content/themes/understrap/inc/queries.php
@@ -6,6 +6,7 @@
 
 function pre_events_query($query) {
 //global $query;
+    if( ! $query->is_main_query()) return;
     if (!isset($query->query["tax_query"][0]["compare"])) {
         if (is_archive() && is_post_type_archive('event') && !is_admin()) {
             $query->set('tax_query', array(
@@ -18,21 +19,31 @@ function pre_events_query($query) {
             $query->set('posts_per_page', -1);
 
         }
-
-        if (is_front_page()) {
-            $query->set('tax_query', array(
-                array(
-                    'taxonomy' => 'eventcategory',
-                    'terms' => 'upcoming-events',
-                    'field' => 'slug'
-                )
-            ));
-            $query->set('posts_per_page', 1);
-
-        }
     }
+
+
+
+    /*
+    if (is_archive() &&  !is_admin()) {
+        echo "<h1>INSIDE FUNCTION</h1>";
+        $var = is_tax( 'member-category','board-member' );
+        var_dump( $var );
+        $query->set('posts_per_page', -1);
+    }
+    */
 
 }
 
 add_action( 'pre_get_posts', 'pre_events_query' );
 
+//has_term( 'board-member', 'member-category' )
+
+
+add_filter( 'pre_get_posts', 'check_for_tax' );
+function check_for_tax( $query ) {
+    //echo "<pre>";print_r($query);
+    if ( $query->is_tax( 'membercategory', 'board-member' )  ) {
+        $query->set("posts_per_page",-1);
+    }
+    //return $query;
+}
