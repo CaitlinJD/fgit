d81515e3d79b122d7fc06906372995b0955f9368
CaitlinJD <caitlin_dyk@hotmail.com>
2016-12-15 09:07:56 -0500

    Deleted archive-event.php and switched it to archive.php

--- wp-content/themes/understrap/archive.php
+++ wp-content/themes/understrap/archive.php
@@ -7,12 +7,12 @@
  * @package understrap
  */
 
-get_header();
-?>
+get_header(); ?>
+
+<h1></h1>
 
 <?php
 $container   = get_theme_mod( 'understrap_container_type' );
-$sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 ?>
 
 <?php get_template_part( 'global-templates/variables', 'none' ); ?>
@@ -24,59 +24,66 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 
 		<div class="row">
 
-			<!-- Do the left sidebar check -->
-			<?php get_template_part( 'global-templates/left-sidebar-check', 'none' ); ?>
-
 			<main class="site-main" id="main">
 
 				<?php if ( have_posts() ) : ?>
 
 					<header class="page-header">
-						<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-						?>
+						<div class="col-xs-12 col-sm-8 col-md-6 no-padding">
+							<?php if ( get_post_type() != 'event') : ?>
+								<!-- Default heading -->
+								<?php
+								the_archive_title( '<h1 class="page-title red-font">', '</h1>' );
+								the_archive_description( '<div class="taxonomy-description">', '</div>' );
+								?>
+
+							<?php else : ?>
+								<!-- Event post type heading -->
+								<?php $category = get_term_by( 'slug', get_query_var('term'), get_query_var('taxonomy') );
+								echo "<h1 class=\"page-title red-font\">".$category->name."</h1>";
+								echo "<p class='taxonomy-description'>".$category->description."</p>";
+								?>
+							<?php endif; ?>
+						</div> <!-- end of col -->
+
 					</header><!-- .page-header -->
 
-					<?php /* Start the Loop */ ?>
-					<?php while ( have_posts() ) : the_post(); ?>
+					<div class="upcoming-events">
+						<?php /* Start the Loop */ ?>
+						<?php while ( have_posts() ) : the_post(); ?>
 
-						<?php
+							<?php
 
-						/*
-						 * Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'loop-templates/content', get_post_format() );
-						?>
+							/*
+                             * Include the Post-Format-specific template for the content.
+                             * If you want to override this in a child theme, then include a file
+                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                             */
+							get_template_part( 'loop-templates/content', get_post_type() );
+							?>
 
-					<?php endwhile; ?>
-
-				<?php else : ?>
-
-					<?php get_template_part( 'loop-templates/content', 'none' ); ?>
+						<?php endwhile; ?>
+					</div>
 
 				<?php endif; ?>
+				<?php
+				if ( is_post_type_archive('event')) {
+					get_template_part('loop-templates/events-past');
 
-			</main><!-- #main -->
+				}
+				?>
 
-			<!-- The pagination component -->
-			<?php understrap_pagination(); ?>
 
-		</div><!-- #primary -->
+			</main> <!-- #main -->
 
-		<!-- Do the right sidebar check -->
-		<?php if ( 'right' === $sidebar_pos || 'both' === $sidebar_pos ) : ?>
 
-			<?php get_sidebar( 'right' ); ?>
+		</div> <!-- .row --> <!-- #primary -->
 
-		<?php endif; ?>
+	</div> <!-- Container end -->
 
-	</div> <!-- .row -->
-
-</div><!-- Container end -->
 
 </div><!-- Wrapper end -->
 
 <?php get_footer(); ?>
+
+
