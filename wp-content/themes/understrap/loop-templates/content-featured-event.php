e0b9d765cf8bca7747b8bcff4b12e29639e48291
CaitlinJD <caitlin_dyk@hotmail.com>
2016-12-16 14:08:45 -0500

    Removed bullet from footer

--- wp-content/themes/understrap/loop-templates/content-featured-event.php
+++ wp-content/themes/understrap/loop-templates/content-featured-event.php
@@ -15,24 +15,15 @@ $query = new WP_Query($args);
         <?php if ($var == 'yes') : ?>
 
             <!-- Add just the featured event -->
-
-                    <div class="img-bkg hidden-xs-down col-sm-5 col-md-6" style="background: url('<?php echo the_post_thumbnail_url('large') ?>') center/cover no-repeat"></div>
-                    <?php if (is_front_page()) : ?>
-                        <div class="event-info col-xs-12 col-sm-5 col-md-6">
-                    <?php endif; ?>
-
-                    <?php echo (is_front_page()? "<h2 class='red-font'>" : ((is_tax('partnercategory', 'mentors'))? "<h1 class='col-xs-12 col-sm-9 col-md-10 red-font'> " : "<h1 class='red-font col-xs-12 col-sm-8 col-md-7'>")); ?>
-                        <?php the_title() ?>
-                    <?php echo (is_front_page()? "</h2>" : "</h1>"); ?>
-
-                    <?php echo ((is_tax('partnercategory', 'mentors')? '<div class="page-content col-xs-10 col-sm-7 col-md-6">' : '' )); ?>
+                <div class="row">
+                    <div class="img-bkg hidden-xs-down col-sm-6 col-md-6" style="background: url('<?php echo the_post_thumbnail_url('large') ?>') center right/cover no-repeat"></div>
+                    <div class="event-info col-xs-12 col-sm-6 col-md-6">
+                        <?php echo (is_front_page()? "<h3 class='red-font'>" : "<h1 class='red-font'>"); ?>
+                            <?php the_title() ?>
+                        <?php echo (is_front_page()? "</h3>" : "</h1>"); ?>
                         <p><?php the_excerpt() ?></p>
-                    <?php echo ((is_tax('partnercategory', 'mentors')? '</div>' : '' )); ?>
-
-                    <?php if (is_front_page()) : ?>
-                        </div>
-                    <?php endif; ?>
-
+                    </div>
+                </div>
                 <?php return; ?> <!-- only let one event be featured -->
 
         <?php endif; ?>
