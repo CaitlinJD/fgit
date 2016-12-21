e2a4528af2410c9fb65dbed9fd946929097ec84a
CaitlinJD <caitlin_dyk@hotmail.com>
2016-12-20 21:17:23 -0500

    Taxonomy queries set to show all posts

--- wp-content/themes/understrap/inc/queries.php
+++ wp-content/themes/understrap/inc/queries.php
@@ -26,12 +26,6 @@ function pre_events_query($query) {
         }
     }
 
-        // Instagram / Gallery query
-        if (is_archive() && is_category('instagram') && !is_admin()) {
-            $query->set('posts_per_page', 12);
-
-        }
-
 }
 
 add_action( 'pre_get_posts', 'pre_events_query' );
@@ -43,5 +37,20 @@ add_filter( 'pre_get_posts', 'check_for_tax' );
 function check_for_tax( $query ) {
     if ( $query->is_tax( 'membercategory', 'board-member' )  ) {
         $query->set("posts_per_page",-1);
+    }
+    if ( $query->is_tax( 'membercategory', 'success-stories' )  ) {
+        $query->set("posts_per_page",-1);
+    }
+    if ( $query->is_tax( 'partnercategory', 'mentors' )  ) {
+        $query->set("posts_per_page",-1);
+    }
+    if ( $query->is_tax( 'partnercategory', 'speakers' )  ) {
+        $query->set("posts_per_page",-1);
+    }
+
+    // Instagram / Gallery query
+    if (is_archive() && is_category('instagram') && !is_admin()) {
+        $query->set('posts_per_page', 12);
+
     }
 }
