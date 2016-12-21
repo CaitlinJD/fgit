135df2980aea49c7afbec774552329f55e8c9d9f
CaitlinJD <caitlin_dyk@hotmail.com>
2016-12-20 20:40:26 -0500

    Styled search results page

--- wp-content/themes/understrap/loop-templates/content-search.php
+++ wp-content/themes/understrap/loop-templates/content-search.php
@@ -13,15 +13,6 @@
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
 		'</a></h2>' ); ?>
 
-		<?php if ( 'post' == get_post_type() ) : ?>
-
-			<div class="entry-meta">
-
-				<?php understrap_posted_on(); ?>
-
-			</div><!-- .entry-meta -->
-
-		<?php endif; ?>
 
 	</header><!-- .entry-header -->
 
@@ -31,10 +22,6 @@
 
 	</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
 
-		<?php understrap_entry_footer(); ?>
-
-	</footer><!-- .entry-footer -->
 
 </article><!-- #post-## -->
