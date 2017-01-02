9c531c150b21d34a819eb63b1518bd681c646839
CaitlinJD <caitlin_dyk@hotmail.com>
2017-01-02 15:32:53 -0500

    Updating facebook links

--- wp-content/plugins/facebook-to-post/facebook-build-data.php
+++ wp-content/plugins/facebook-to-post/facebook-build-data.php
@@ -19,8 +19,7 @@ class raw_facebook_build extends facebook_call {
       $item['facebook_post_image'] = $facebook_post->full_picture;
       $item['facebook_post_story'] = $facebook_post->story;
       $item['facebook_post_id'] = $facebook_post->id;
-      $item['facebook_post_url'] =  "https://www.facebook.com/fgi.toronto/posts/".$facebook_post->id;
-
+      $item['facebook_post_url'] =  "https://www.facebook.com/".$facebook_post->id;
       $post = new facebook_to_post;
       $post->facebook = $item;
       $post->import_facebook_posts();
