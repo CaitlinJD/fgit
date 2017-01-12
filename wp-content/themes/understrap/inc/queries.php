b4a7e6745b18bec33b775c9bb8dc072d1bd073cc
miasalav <miaria00@gmail.com>
2017-01-11 21:55:11 -0500

    Pushing queries.php

--- wp-content/themes/understrap/inc/queries.php
+++ wp-content/themes/understrap/inc/queries.php
@@ -6,6 +6,10 @@
 
 function pre_events_query($query) {
 //global $query;
+    $paged = get_query_var( 'paged' );
+    $page = ( !$paged ? 1 : $paged );
+    $query->set('paged', $page );
+
     if( ! $query->is_main_query()) return;
     if (!isset($query->query["tax_query"][0]["compare"])) {
         // Second query for events archive
@@ -21,6 +25,7 @@ function pre_events_query($query) {
 
         }
     }
+
 }
 
 add_action( 'pre_get_posts', 'pre_events_query' );
@@ -32,5 +37,20 @@ add_filter( 'pre_get_posts', 'check_for_tax' );
 function check_for_tax( $query ) {
     if ( $query->is_tax( 'membercategory', 'board-member' )  ) {
         $query->set("posts_per_page",-1);
+    }
+    if ( $query->is_tax( 'membercategory', 'success-stories' )  ) {
+        $query->set("posts_per_page",-1);
+    }
+    if ( $query->is_tax( 'partnercategory', 'mentors' )  ) {
+        $query->set("posts_per_page",-1);
+    }
+    if ( $query->is_tax( 'partnercategory', 'speakers' )  ) {
+        $query->set("posts_per_page",-1);
+    }
+
+    // Instagram / Gallery query
+    if (is_archive() && is_category('instagram') && !is_admin()) {
+        $query->set('posts_per_page', 12);
+
     }
 }
